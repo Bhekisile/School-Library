c2e5965951d5908cf0d7ad30ade9a64b04ac54ef
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-10-09 12:30:04 +0200

    Refactor app file

---
+++ list.rb
@@ -0,0 +1,35 @@
+class List
+  def list_all_books(books)
+    puts 'Books unavailable' if books.empty?
+    puts
+    books.each do |book|
+      puts "Title: #{book.title}, Author: #{book.author}"
+    end
+  end
+
+  def list_all_people(people)
+    puts 'List is empty' if people.empty?
+    puts
+    people.values.each do |person|
+      puts "[#{person.class}] ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+      puts
+    end
+  end
+
+  def list_all_rentals(people)
+    print 'ID of person: '
+    person_id = gets.chomp.to_i
+
+    person_obj = people.values.find { |person| person.id == person_id }
+
+    if person_obj.nil?
+      puts 'Person not found'
+      return
+    end
+
+    puts 'Rentals: '
+    person_obj.rentals.each do |rental|
+      puts "Date: #{rental.date} Book: #{rental.book.title} by #{rental.book.author}"
+    end
+  end
+end
---
+++ teacher_app.rb
@@ -0,0 +1,15 @@
+require_relative 'teacher'
+
+class TeacherApp
+  def create_teacher(people)
+    print 'Name: '
+    name = gets.chomp
+    print 'Age: '
+    age = gets.chomp.to_i
+    puts 'Specialization'
+    specialization = gets.chomp
+    teacher = Teacher.new(age, specialization, name)
+    people[teacher.id] = teacher
+    puts 'Teacher created successfully'
+  end
+end
